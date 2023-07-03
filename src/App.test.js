fc41fc000a788077b2bc8d3581407891de1027ce
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-07-02 23:30:02 -0700

    tests: added in new test to test for error handling

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,23 @@ import { screen, render, fireEvent, within } from '@testing-library/svelte';
 import App from './App';
 
 describe('App', () => {
-  render(App);
-
   test('Should write a full name into Preview', async () => {
+    render(App);
+
     const cardNameInputField = screen.getByPlaceholderText('e.g. Jane Appleseed');
     
     await fireEvent.input(cardNameInputField, { target: { value: 'John Doe' }});
 
     expect(screen.getByText('John Doe')).toHaveTextContent('John Doe');
+  });
+
+  test('Should display an error if CC is entered in wrong format', async () => {
+    render(App);
+
+    const cardNumberInputField = screen.getByPlaceholderText('e.g. 1234 5678 9123 0000');
+
+    await fireEvent.input(cardNumberInputField, { target: { value: 'ii12adui8s' } });
+
+    expect(screen.getByText('Wrong format, numbers only')).toHaveTextContent('Wrong format, numbers only');
   });
 })
